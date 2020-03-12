c7dd92844a2327d21d141a5d009453b924868d83
Thomas Dem <demagny.t@hotmail.fr>
2020-03-12 17:09:33 +0100

    codacy bug fix

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,8 +1,7 @@
 (function($) {
-    "use strict"; // Start of use strict
-
+    "use strict";
     // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    $("a.js-scroll-trigger[href*='#']:not([href='#'])").click(function () {
         if (location.pathname.replace(/^\//, "") === this.pathname.replace(/^\//, "") && location.hostname === this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $("[name=' + this.hash.slice(1) + ']");
